d96c419f91f66960614764de23a9d8def551c83e
John Azzaro <jazzaro@temple.edu>
2019-05-21 20:19:23 -0700

    initial commit

---
+++ servSideProgOutline.js
@@ -0,0 +1,42 @@
+"use strict";
+// TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
+//     Server Side Programming Outline
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//     1. 
+// NOTES ////////////////////////////////////////////////////////////////////////////////////////////////////
+//     1. Useful overview of information on Server side programming (in JavaScript) from study, research, tutorials, 
+//        mentor meetings, peer discussions, and good ole' fashioned curiosity.  I've put the document in Question
+//        and Answer format for improved readability.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* 
+Preliminary outline
+
+•	Introduction to Server side programming
+    o	What is a server side programming?
+    o	What is a web app?
+    o	What is a web server?
+    o	What are resources and how to web apps expose them?
+    o	How do web servers and clients communicate?
+    o	How do Web servers route requests to the right handlers?
+    o	Do web servers house the data base layer?
+    o	What is the importance of business logic to web applications?
+    o	What is software testing?
+
+•	Introduction to Node and Express
+    o	What is node?
+    o	What is express?
+    o	What is Glitch and how do we utilize it in server side programming?
+    o	How does a basic application using node and express work?
+    o	Basic application example using node and express: counter app 
+
+•	Web servers
+    o	What is a dynamic asset?
+    o	How do you serve a dynamic asset?
+    o	What is a static asset?
+    o	How do you serve a static asset?
+    o	Basic example: need to think of something.
+
+
+*/
