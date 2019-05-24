cf03982af9988c46ff40c3a66a3dc61df12ad913
John Azzaro <jazzaro@temple.edu>
2019-05-24 08:27:07 -0700

    foundational edits

--- fundaServSideProg.js
+++ fundaServSideProg.js
@@ -11,31 +11,52 @@
 //     1. Useful overview of information on Server side programming (in JavaScript) from study, research, tutorials, 
 //        mentor meetings, peer discussions, and good ole' fashioned curiosity.  I've put the document in Question
 //        and Answer format for improved readability.
-//
+// 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* 
+https://mdn.mozillademos.org/files/13839/Web%20Application%20with%20HTML%20and%20Steps.png
+
+    •	
+
+*/
+
+
 
 /* 
-•	Int
-    o	int
+1. What is a web server?
+/////////////////////////
+==SHORT ANSWER==
+    •	
 
+==EXTENDED ANSWER==
+    •	
+*/
+
+
+/* 
 1. What is a server-side web app?
 //////////////////////////////////
-•	A server-side web app is a program whose purpose is to EXPOSE RESOURCES to other applications.
+==SHORT ANSWER==
+    •	A server-side web app is a PROGRAM whose PRUPOSE is to EXPOSE RESOURCES to other applications.
+*/
 
-    What can a server-side web app do?
-    ==================================
-    •	A single web server can support many CLIENTS.
-    •	Clients on a web server can be: WEB BROWSERS, MOBILE APPS, OTHER SERVERS, etc.	
+
+/*
+2. What is a server-side client?
+////////////////////////////////
+==SHORT ANSWER==
+    •   A single web server can support many CLIENTS such as WEB BROWSERS, MOBILE APPS, OTHER SERVERS, etc.	
 */
 
 
 /* 
 2. What is a client application?
 /////////////////////////////////
-•	A client application REQUESTS RESOURCES from a server.
+==SHORT ANSWER==
+    •	A client application REQUESTS RESOURCES from a SERVER.
 
-    EXAMPLE: WikiPedia API
-    =======================
+    How would a basic client application work?
+    ==========================================
     o	For example, if you build an application that searches Wikipedia via an API, the application you
         have built is a client application.
         o   The CLIENT APPLICATION requests data from a server (i.e. the API).                          [CA]===> [Server]
@@ -43,14 +64,19 @@
         o   Client applications can also SEND data to a server so it can be stored or processed.        [CS]==(Data)=> [Server]
 */
 
+
 /*
 3. What are resources?
 //////////////////////
+==SHORT ANSWER==
+    •	Resources are 
+
 •   Resources are things such as:
     o   index.html page                  [.html]
     o   data represesnting customers     {name:"joe"}
     o   main.css page                    [.css]
 */
+
 
 /* 
 4. What kind of resources can server-side apps expose?
