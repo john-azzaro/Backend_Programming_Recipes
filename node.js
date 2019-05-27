aaea8751072359b45eaa9512eda8b86c944cc72b
John Azzaro <jazzaro@temple.edu>
2019-05-26 22:59:47 -0700

    node

--- node.js
+++ node.js
@@ -10,3 +10,32 @@
 //        Answer format for improved readability.
 // 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+1. What is node.js?
+///////////////////
+==SHORT ANSWER==
+    •   Node.js is a platform that allows us to write JavaScript that runs on the server.
+
+==EXTENDED ANSWER==
+    •   
+
+*/
+
+
+/* 
+3. Why do you need to run JavaScript?
+///////////////////////////
+    •   
+*/
+
+
+/*
+2. How does node.js work?
+//////////////////////////
+    •   Node.js uses V8 JavaScript engine in chrome.
+    •   V8 is a "just-in-time compiler" written in C++ that takes the JavaScript you write in your web apps
+        and turns it into machine-level instructions.
+*/
+
+
