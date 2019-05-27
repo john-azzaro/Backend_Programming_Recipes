17782aa2113fba27473c713f5334e5cd8a654b3b
John Azzaro <jazzaro@temple.edu>
2019-05-26 23:13:02 -0700

    node additions

--- node.js
+++ node.js
@@ -16,6 +16,8 @@
 ///////////////////
 ==SHORT ANSWER==
     •   Node.js is a platform that allows us to write JavaScript that runs on the server.
+    •   Node.js uses the chrome V8 engine to give us a platform to execute JavaScript outside the browser. 
+    •   Node.js is a server-side enviroment for executing JavaScript.
 
 ==EXTENDED ANSWER==
     •   
@@ -24,9 +26,13 @@
 
 
 /* 
-3. Why do you need to run JavaScript?
-///////////////////////////
-    •   
+3. Why do you need node to run JavaScript on a server?
+//////////////////////////////////////////////////////
+    •   Computers execute machine code insstructions .
+        o   HOWEVER, machine code is difficult for humans to work with.
+        o   Programming languages like Node to abstract away from machine code to make it easier for people to use.
+
+       
 */
 
 
@@ -36,6 +42,7 @@
     •   Node.js uses V8 JavaScript engine in chrome.
     •   V8 is a "just-in-time compiler" written in C++ that takes the JavaScript you write in your web apps
         and turns it into machine-level instructions.
+    •   A JavaScript engine translates JavaScript into instructions that the computer can execute.
 */
 
 
