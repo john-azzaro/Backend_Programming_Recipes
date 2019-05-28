8a5078fa4a5ae467ed6e7da2bcaaf34748466c99
John Azzaro <jazzaro@temple.edu>
2019-05-28 16:22:49 -0700

    additional global object examples

--- nodeModules.js
+++ nodeModules.js
@@ -15,16 +15,43 @@
 
 
 /* 
-1. What are global objects?
-///////////////////////////
-    •   A global object is part of the global scope which means we can access it anywhere in our code.
+1. What are global objects in JavaScript?
+/////////////////////////////////////////
+==SHORT ANSWER
+    •   A global object is part of the global scope which means we can access it anywhere in our code.  There 
+        are also objects that are available in node as well which can be used on both client and server side.
+
+==EXAMPLE==
         o   For example, console.log(); is a global object
 
-    •   Other objects that are available in node as well which can be used on both client and server side, such as:
-        o   setTimeout() -- This calls a function after a delay like 1 or 2 seconds.
-        o   clearTimeOut() -- This clears a timeout which has been set before a setTimeout function that comes before it.
+==EXTENDED ANSWER==
 
-        o   setInterval() -- 
+    •   In browsers, there is a "window" object that represents global scope.
+        o   So all the variables and functions defined globally can be accessed by the window object
+                    
+                    console.log()  
+
+           o    However, the JavaScript engine will prefix this statement with 'window' because that is where is 
+                object is defined, 
+                
+                    window.console.log().
+
+    •   As part fo the window object, there are other objects that are available in node as well which can be 
+        used on both client and server side, such as:
+
+            o   setTimeout() -- This calls a function after a delay like 1 or 2 seconds.
+            o   clearTimeOut() -- This clears a timeout which has been set before a setTimeout function that comes before it.
+
+            o   setInterval() -- Same as setTImeout, but repeatedly calls a function after a given delay.
+            o   clearInterval() -- Used to stop a function from being called repeatedly.
+
+    •   And since these belong to the window object:
+
+                    window.setTimeout()
+                    window.clearTimeout()
+                    window.setInterval()
+                    window.clearInterval()
+
 
 */
 
