71574d857f541db9eee1580b316b845e1b0c6e81
John Azzaro <jazzaro@temple.edu>
2019-05-28 16:01:07 -0700

    global object examples

---
+++ nodeModules.js
@@ -0,0 +1,67 @@
+"use strict";
+// TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
+//     Node Modules
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//      1.    
+//
+// NOTES ////////////////////////////////////////////////////////////////////////////////////////////////////
+//     1. Useful overview of information on Node Modules from study, research, tutorials, mentor meetings,
+//        peer discussions, and good ole' fashioned curiosity.  I've put the document in Question and
+//        Answer format for improved readability.
+//     3. Download node.js: https://nodejs.org/en/
+//     2. Documentation for Node.js: https://nodejs.org/en/docs/
+// 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/* 
+1. What are global objects?
+///////////////////////////
+    •   A global object is part of the global scope which means we can access it anywhere in our code.
+        o   For example, console.log(); is a global object
+
+    •   Other objects that are available in node as well which can be used on both client and server side, such as:
+        o   setTimeout() -- This calls a function after a delay like 1 or 2 seconds.
+        o   clearTimeOut() -- This clears a timeout which has been set before a setTimeout function that comes before it.
+
+        o   setInterval() -- 
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+https://medium.com/@mattburgess/beyond-console-log-2400fdf4a9d8 -- console
+https://www.geeksforgeeks.org/javascript-cleartimeout-clearinterval-method/ -- clearTimeout explaination
+*/
+
+
+
