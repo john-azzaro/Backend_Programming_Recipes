f3e3b06eeeea98454122663f7c0659738ee22217
John Azzaro <jazzaro@temple.edu>
2019-06-03 22:09:34 -0700

    package.json bascis

--- npm.js
+++ npm.js
@@ -7,6 +7,8 @@
 //         +  How do you verify your NPM installation? 
 //         +  How do you update NPM to the latest version?
 //         +  How do you access npm help? 
+//         +  How do you install a specific version of npm?
+//      2. What is package.json?
 //
 // NOTES ////////////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Useful overview of information on Node Package Manager from study, research, tutorials, mentor
@@ -45,11 +47,83 @@
     How do you install a specific version of npm?
     =============================================
     •   write "npm i -g npm@5.5.1"
-    
+
             o   "i" stands for install
             o   -g stands for global
             o   "npm@5.5.1" is the package and version number
- 
+*/
+
+
+
+/* 
+What is package.json?
+////////////////////////
+==EXTENDED ANSWER==
+    •   Package.json is a JSON file that include basic information about your application.
+            o   When created, the package.json file will have a JSON object with key/value pairts
+    •   Before you create any files in your project you need to create a package.json.
+    •   All applications have as a standard the package.json file.
+    •   Package.json includes "metadata" information for your application such as:
+            o   name
+            o   version
+            o   authors
+            o   git repo address
+            o   dependencies
+            o   etc.
+
+
+==PRACTICAL EXAMPLE==
+
+    How do you create a package.json file?
+    ======================================
+        STEP 1: in Gitbash, write "npm init"
+        ====================================
+
+        1.      $ npm init
+
+        STEP 2: When you run "npm init", you will be guided through the process of creating a package.json file.
+        ========================================================================================================
+
+                $ npm init
+                This utility will walk you through creating a package.json file.
+                It only covers the most common items, and tries to guess sensible defaults.
+
+                See `npm help json` for definitive documentation on these fields
+                and exactly what they do.
+
+                Use `npm install <pkg>` afterwards to install a package and
+                save it as a dependency in the package.json file.
+
+                Press ^C at any time to quit.
+        2.      package name: (npm-demo)                                               <== Name of your project (file name of npm-demo by default)
+        3.      version: (1.0.0)                                                       <== Project version number
+        4.      description:                                                           <== description of project
+        5.      entry point: (index.js)
+        6.      test command:
+        7.      git repository:
+        8.      keywords:
+        9.      author:
+        10.     license: (ISC)
+                About to write to C:\Users\Admin\Desktop\npm-demo\package.json:
+
+                {
+                "name": "npm-demo",
+                "version": "1.0.0",
+                "description": "",
+                "main": "index.js",
+                "scripts": {
+                    "test": "echo \"Error: no test specified\" && exit 1"
+                },
+                "author": "",
+                "license": "ISC"
+                }
+
+        11.     Is this ok? (yes)                                                        <== creates a package.json file in your project folder
+
+
+        OPTIONAL: If you want to bypass these questions and make a package.json file in a rush
+        ======================================================================================
+            •   write npm init --yes
 
 
 */
