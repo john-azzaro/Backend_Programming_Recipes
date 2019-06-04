3b8522e074096533b763caa41b3dbdae956b33f7
John Azzaro <jazzaro@temple.edu>
2019-06-03 21:48:12 -0700

    QA NPM

--- npm.js
+++ npm.js
@@ -2,7 +2,11 @@
 // TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
 //     Node Package Manager (NPM)
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//      1.   
+//      1. What is Node Package Manager (NPM)?
+//         +  How do you install NPM? 
+//         +  How do you verify your NPM installation? 
+//         +  How do you update NPM to the latest version?
+//         +  How do you access npm help? 
 //
 // NOTES ////////////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Useful overview of information on Node Package Manager from study, research, tutorials, mentor
@@ -14,12 +18,13 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /* 
-What is Node Package Manager (NPM)?
-///////////////////////////////////
+1. What is Node Package Manager (NPM)?
+//////////////////////////////////////
     •   Node Package Manager, or NPM, is a command line tool (such as using Gitbash) is a command line tool that 
         installs, updates, or uninstalls Node.js packages in your application.
             o  NPM is also an online repository for open-source Node.js packages where the community creates useful modules
                and published them as packages to the repository.
+            o  NPM and Node are developed independently, so if you check the version of npm and node, they will be different.
 
     How do you install NPM?
     =======================
@@ -27,8 +32,24 @@ What is Node Package Manager (NPM)?
 
     How do you verify your NPM installation?
     ========================================
-    •   To verify your NPM installation, simple writing "npm -v" in your Gitbash terminal.
-            o   
+    •   write "npm -v" in your Gitbash terminal.
+     
+    How do you update NPM to the latest version?
+    ============================================
+    •   write "npm install npm -g".
+
+    How do you access npm help?
+    ===========================
+    •   write "npm help".
+
+    How do you install a specific version of npm?
+    =============================================
+    •   write "npm i -g npm@5.5.1"
+    
+            o   "i" stands for install
+            o   -g stands for global
+            o   "npm@5.5.1" is the package and version number
+ 
 
 
 */
