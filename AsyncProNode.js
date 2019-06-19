1aed234a26bcbb7f0e1dc818dbb1e9106ba852d8
John Azzaro <jazzaro@temple.edu>
2019-06-18 21:57:41 -0700

    async vs sync

--- AsyncProNode.js
+++ AsyncProNode.js
@@ -16,11 +16,21 @@
 /* 
 What is the difference between aysnchronous vs synchronous programming?
 ///////////////////////////////////////////////////////////////////////
-    •   Synchronous, or "blocking", programming, where a program will process one thing first and then process a second.
+    •   "Synchronous", or "blocking", programming, where a program will process one thing first and then process a second.
         o   For example:
 
-                        console.log('Before');  <==  When this first line executes, the program in "blocking" the second line which has to wait.
-                        console.log('After');   <==
+                        console.log('Before');       <==  When this first line executes, the program in "blocking" the second line which has to wait.
+                        console.log('After');        <==  This line can execute only AFTER the first line executes.
 
-    •   
+                        o   This program would be "synchronous" or "blocking" because one line must finish executing
+                            before the next line can execute.
+
+
+    •   "Asynchronous", or "non-blocking" programming, is where a program 
+
+                        setTimeout(function() {
+
+                        }, 2000);
 */
+
+console.log('Before');
