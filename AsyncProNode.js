ac72e15cb5a93ce7e59dd02b4ba5ed5cb107e892
John Azzaro <jazzaro@temple.edu>
2019-06-20 08:35:35 -0700

    async functionality

--- AsyncProNode.js
+++ AsyncProNode.js
@@ -2,7 +2,7 @@
 // TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
 //     Asynchronous Programming in Node
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//      1. 
+//      1. What is the difference between aysnchronous vs synchronous programming?
 //      
 //
 // NOTES ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -17,11 +17,11 @@
 
 
 /* 
-What is the difference between aysnchronous vs synchronous programming?
-///////////////////////////////////////////////////////////////////////
+1. What is the difference between aysnchronous vs synchronous programming?
+//////////////////////////////////////////////////////////////////////////
     •   "Synchronous", or "blocking", where a program will process one thing first and then process a second.
         o   For example:
-        
+
 
                         console.log('Before');       <==  When this first line executes, the program in "blocking" the second line which has to wait.
                         console.log('After');        <==  This line can execute only AFTER the first line executes.
@@ -40,14 +40,28 @@ What is the difference between aysnchronous vs synchronous programming?
                             console.log('Reading a user from a database...')                           
                         }, 2000);
 
-                        console.log('After');                           //=>    Before
-                                                                                After
-                                                                                Reading a user from a database...
-          
+                        console.log('After');                           
+                                                
+                        
+        o   The program will execute as follows:
+
+                        1. First "Before" is displayed in the console...
+                        2. Second, setTimeout "schedules" a function to be called after 2 seconds...
+                        3. Third, "After" is displayed in the console...
+                        4. Fourth, after 2 seconds, the setTimeout executes displaying "Reading a user from a database..."
+
+                                $ node index.js
+                                Before
+                                After
+                                Reading a user from a database...
+
 
         o   What happens is that since the setTime function is "asynchronous", when the function is called it will schedule
             a task to be performed in the future, specficially 2 seconds in the future.
-        o   The program does NOT wait or BLOCK the rest of the program from executing
+        o   The program does NOT wait or BLOCK the rest of the program from executing.
+        o   HOWEVER, asynchronous does NOT mean "concurrent" or "multithreaded"... the example above is a single thread.
+        o   Asynchronous functionality is useful to know when you are working with Node programming when you are dealing with
+            disk or network access.
 
 
 
