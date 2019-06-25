424bb1f5f8fcd2a6073ae0ff698bf536be584e76
John Azzaro <jazzaro@temple.edu>
2019-06-25 09:35:54 -0700

    completed formatting reset name and summary block

--- MongoDB.js
+++ MongoDB.js
@@ -2,41 +2,11 @@
 // TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
 //     MongoDb
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//      1. What is Node Package Manager?. 
-//         +  How do you install NPM?
-//         +  How do you verify your NPM installation?
-//         +  How do you install NPM to the latest version?
-//         +  How do you access npm help?
-//         +  How do you install a specific version of npm?
-//      2. What is package.json?
-//         +  How do you create a package.json file?
-//         +  OPTIONAL: Is there a way to create a package.json file quickly? 
-//      3. How do you install an npm package?
-//         +  How do you install a specific version of a package?
-//         +  How do you find out which packages are outdated?
-//         +  How do you update a package to the latest version of minor and patch releases?
-//         +  How do you update to the latest version of major releases using ncu?
-//      4. How do you uninstall a npm package?
-//      5. How do you use a npm module? 
-//      6. What are package dependencies in package.json?
-//         +  What is semantic versioning?
-//         +  How do you view the registry of your package on Gitbash?
-//         +  How do you view ONLY dependencies of your package on Gitbash?
-//         +  How do you know what package version is installed?
-//         +  How do you check for all the versions of a dependencies?
-//      7. What are DevDependencies and what do they do?
-//         +  How do you install a DevDependency?
-//      8. What is .gitignore and why do you need it with version control?
-//         +  How do you exclude the node_modules file from your git repository?
-//         +  How do you restore dependencies?
-//      9. How do you publish your own node package and publish on npm registry?
-//     10. How do you update an uploaded node package and publish on npm registry?
+//      1. 
 //
 // NOTES ////////////////////////////////////////////////////////////////////////////////////////////////////
-//     1. Useful overview of information on Node Package Manager from study, research, tutorials, mentor
-//        meetings, peer discussions, and good ole' fashioned curiosity.  I've put the document in Question
-//        andAnswer format for improved readability.
-//     3. Download node.js: https://nodejs.org/en/
-//     2. Documentation for Node.js: https://nodejs.org/en/docs/
+//     1. Useful overview of information on MongoDB from study, research, tutorials, mentor meetings,
+//         peer discussions, and good ole' fashioned curiosity.  I've put the document in Question
+//        and Answer format for usefulness and improved readability.
 // 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
