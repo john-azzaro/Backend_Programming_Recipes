150b17fcf0308d6f711d0fc45948ea9b44060173
John Azzaro <jazzaro@temple.edu>
2019-06-27 11:11:40 -0700

    QA refactoring

--- AsyncProNode.js
+++ AsyncProNode.js
@@ -41,32 +41,27 @@
         o   For example:
 
 
-                        console.log('Before');       <==  When this first line executes, the program in "blocking" the second line which has to wait.
-                        console.log('After');        <==  This line can execute only AFTER the first line executes.
+                                console.log('Before');       <==  When this first line executes, the program in "blocking" the second line which has to wait.
+                                console.log('After');        <==  This line can execute only AFTER the first line executes.
 
-                        o   This program would be "synchronous" or "blocking" because one line must finish executing
-                            before the next line can execute.
+                                                                o   NOTE: This program would be "synchronous" or "blocking" because  
+                                                                    one line must finish executing before the next line can execute.
 
 
     •   "Asynchronous", or "non-blocking", architecture is where a single thread services multiple requests.
         o   For this example, we'll use a timeout, which will executes a function after a given amount of time.
 
 
-                        console.log('Before')
+                                console.log('Before')                                   <== 1. First "Before" is displayed in the console...
 
-                        setTimeout(function() {                                    
-                            console.log('Reading a user from a database...')                           
-                        }, 2000);
+                                setTimeout(function() {                                 <== 2. Second, setTimeout "schedules" a function to be called after 2 seconds...       
+                                    console.log('Reading a user from a database...')                            
+                                }, 2000);   
 
-                        console.log('After');                           
-                                                
+                                console.log('After');                                   <== 3. Third, "After" is displayed in the console...
+                                                                                        <== 4. Fourth, after 2 seconds, setTimeout executes "Reading a user from a database..."
                         
-        o   The program will execute as follows:
-
-                        1. First "Before" is displayed in the console...
-                        2. Second, setTimeout "schedules" a function to be called after 2 seconds...
-                        3. Third, "After" is displayed in the console...
-                        4. Fourth, after 2 seconds, the setTimeout executes displaying "Reading a user from a database..."
+                    o   The program will execute as follows:
 
                                 $ node index.js
                                 Before
@@ -74,12 +69,12 @@
                                 Reading a user from a database...
 
 
-        o   What happens is that since the setTime function is "asynchronous", when the function is called it will schedule
-            a task to be performed in the future, specficially 2 seconds in the future.
-        o   The program does NOT wait or BLOCK the rest of the program from executing.       
-        o   HOWEVER, asynchronous does NOT mean "concurrent" or "multithreaded"... the example above is a single thread.
-        o   Asynchronous functionality is useful to know when you are working with Node programming when you are dealing with
-            disk or network access.
+                    o   Since the setTime function is "asynchronous", when the function is called it will schedule 
+                        a task to be performed in the future, specficially 2 seconds in the future.
+                            o   The program does NOT wait or BLOCK the rest of the program from executing.       
+                            o   HOWEVER, asynchronous does NOT mean "concurrent" or "multithreaded"... the example above is a single thread.
+                            o   Asynchronous functionality is useful to know when you are working with Node programming when you are dealing with
+                                disk or network access.
 */
 
 
@@ -88,12 +83,18 @@
 2. What patterns can you use to deal with asynchronous code and why would you need them?
 /////////////////////////////////////////////////////////////////////////////////////
 ==SHORT ANSWER==
-    •   The three patterns that help you deal with asynchronous code are: Callbacks, Promises, and Async/Await.
-    •   Why would you need these patterns? When you try to access something like a database, the result will not be 
-        available immediately.  It might take a few seconds to retrieve the information so you need something like
-        these patterns to successfully retrieve the information.
+    •   The three patterns that help you deal with asynchronous code are: 
+            o   Callbacks, 
+            o   Promises,
+            o   Async/Await.
 
-==EXTENDED ANSWER==
+    •   You need these patterns because when you try to access something like a database, the result will not be 
+        available immediately.  
+            o   It might take a few seconds to retrieve the information so you need something like
+                these patterns to successfully retrieve the information.
+
+
+==EXAMPLE==
     •   Take for example the following program which will unsuccessfully retrieve the information within the
         getUser function because of the extended execution of the setTimeout function within it.
 
@@ -111,19 +112,22 @@
                                 }
 
                         
-        o   The program above will display the following:                        
+                    o   The program above will display the following:                        
 
                                 Before
-                                undefined                          <== Note that "undefined" is NOT the user object { id: id, gitHubUsername: 'joe' }
+                                undefined                            <== Note that "undefined" is NOT the user object { id: id, gitHubUsername: 'joe' }
                                 After
                                 Reading a user from a database...
 
-        o   The reason "undefined" is returned instead of the specific user in the getUser function is because the 
-            function within the setimeout function is executed 2 seconds after intial execution. 
-        o   In other words, the function will NOT be available at the time of calling getUser() to show in the console, so it will
-            return as undefined.
-        o   To avoid this issue, we use three patterns to help deal with asynchrnous code: Callbacks, Promises, and Async/Await.
+                    o   The reason "undefined" is returned instead of the specific user in the getUser function is 
+                        because the function within the setimeout function is executed 2 seconds after intial execution. 
+                            o   In other words, the function will NOT be available at the time of calling getUser() to show in 
+                                the console, so it will return as undefined.
+                            o   To avoid this issue, we use three patterns to help deal with asynchrnous code: Callbacks, Promises, 
+                                and Async/Await.
 */
+
+
 
 /* 
 3. What is a callback and how do you use it?
