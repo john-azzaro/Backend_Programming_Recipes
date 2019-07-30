9d30b62bbc75ea6f393315763ea5a39a804426e5
John Azzaro <jazzaro@temple.edu>
2019-07-30 16:57:00 -0700

    reordered express sections,  restored REST, etc.

--- express.js
+++ express.js
@@ -10,10 +10,6 @@
 //        +  How do you listen on a given port for requests?         
 //        +  How do you add another route to your server?
 
-//     2. What is Nodemon and what does it do?    
-//        +  How do you install nodemon?
-//        +  How do you use nodemon in Gitbash?
-
 //     3. What is an environment variable and how do you assign a port to your node application?
 //        +  How do you set an enviroment variable (assign a port to your application?
 //
@@ -190,46 +186,6 @@
 */
 
 
-
-
-
-
-/* 
-2. What is Nodemon and what does it do?
-///////////////////////////////////////
-    •   Nodemon (short for Node Monitor) is an npm package that we install that allows us to update our node program
-        without having to start and restart our server every time we make a change in out program.
-
-    How do you install nodemon?
-    ===========================
-    •   To install nodemon, you simple write "npm install -g nodemon"
-        o   What will happen is is you will install the npm package "nodemon" but globally so it will be available from now on.
-
-    How do you use nodemon in Gitbash?
-    ==================================
-    •   When you use nodemon, you simply type "nodemon" and then the application you want to listen in on.
-
-                    $ nodemon index.js                                   <== uses nodemon with index.js.
-                        [nodemon] 1.19.1
-                        [nodemon] to restart at any time, enter `rs`
-                        [nodemon] watching: *.*                          <== nodemon watches everything in the folder.
-                        [nodemon] starting `node index.js`
-                        listening on port 3000...                        <== listening in on port 3000, which will show "Hello world!"
-
-
-    •   Then, when you update your program (i.e. the response is "Howdy world!"), nodemon will automatically restart.
-
-                    $ nodemon index.js
-                        [nodemon] 1.19.1
-                        [nodemon] to restart at any time, enter `rs`
-                        [nodemon] watching: *.*   
-                        [nodemon] starting `node index.js`
-                        listening on port 3000...
-                        [nodemon] restarting due to changes...           <== nodemon restarts after the change...
-                        [nodemon] starting `node index.js`
-                        listening on port 3000...                        <== and again listens on port 3000, and when requested will show "Howdy world!"
-
-*/
 
 
 
